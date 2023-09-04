c76843440fd05e6659072cd8ead6184ec3eb37b3
ypmen <ypmen@mpifr-bonn.mpg.de>
2023-09-04 20:03:11 +0200

    add pol in histof

--- src/slow_transient/histof.cpp
+++ src/slow_transient/histof.cpp
@@ -16,6 +16,7 @@ using namespace boost::program_options;
 #include "psrdatareader.h"
 #include "psrfitsreader.h"
 #include "filterbankreader.h"
+#include "stat.h"
 
 unsigned int num_threads;
 
@@ -32,6 +33,7 @@ int main(int argc, const char *argv[])
 			("verbose,v", "Print debug information")
 			("threads,t", value<unsigned int>()->default_value(1), "Number of threads")
 			("zapthre", value<float>()->default_value(3), "Threshold in IQR for zapping channels")
+			("nosumif", "Keep polarization")
 			("cont", "Input files are contiguous")
 			("wts", "Apply DAT_WTS")
 			("scloffs", "Apply DAT_SCL and DAT_OFFS")
@@ -85,6 +87,8 @@ int main(int argc, const char *argv[])
 
 	std::vector<std::string> fnames = vm["input"].as<std::vector<std::string>>();
 
+	bool nosumif = vm.count("nosumif");
+
 	PSRDataReader * reader;
 
 	if (vm.count("psrfits"))
@@ -93,7 +97,7 @@ int main(int argc, const char *argv[])
 		reader= new FilterbankReader;
 
 	reader->fnames = fnames;
-	reader->sumif = true;
+	reader->sumif = !nosumif;
 	reader->contiguous = contiguous;
 	reader->verbose = verbose;
 	reader->apply_scloffs = apply_scloffs;
@@ -110,128 +114,30 @@ int main(int argc, const char *argv[])
 
 	int ndump = 1024;
 
-	DataBuffer<float> databuf(ndump, nchans);
+	DataBuffer<float> databuf;
+	if (nosumif)
+		databuf.resize(ndump, nifs * nchans);
+	else
+		databuf.resize(ndump, nchans);
 
-	std::vector<float> chmean(nchans, 0.);
-	std::vector<float> chstd(nchans, 0.);
-	std::vector<float> chskewness(nchans, 0.);
-	std::vector<float> chkurtosis(nchans, 0.);
-	std::vector<float> chweight(nchans, 0.);
+	Stat stat;
+	stat.zap_threshold = vm["zapthre"].as<float>();
+	stat.prepare(databuf);
 
-	std::vector<double> chmean1(nchans, 0.);
-	std::vector<double> chmean2(nchans, 0.);
-	std::vector<double> chmean3(nchans, 0.);
-	std::vector<double> chmean4(nchans, 0.);
-	std::vector<double> chcorr(nchans, 0.);
-	std::vector<double> last_data(nchans, 0.);
-
-	size_t count = 0;
 	while (!reader->is_end)
 	{
 		if (reader->read_data(databuf, ndump) != ndump) break;
 
-		for (size_t i=0; i<databuf.nsamples; i++)
-		{
-			for (size_t j=0; j<databuf.nchans; j++)
-			{
-				double tmp1 = databuf.buffer[i * databuf.nchans + j];
-				double tmp2 = tmp1*tmp1;
-				double tmp3 = tmp2*tmp1;
-				double tmp4 = tmp2*tmp2;
-				chmean1[j] += tmp1;
-				chmean2[j] += tmp2;
-				chmean3[j] += tmp3;
-				chmean4[j] += tmp4;
-
-				chcorr[j] += tmp1*last_data[j];
-				last_data[j] = tmp1;
-			}
-		}
-
-		count += ndump;
+		stat.run(databuf);
 	}
 
-	for (long int j=0; j<databuf.nchans; j++)
-	{
-		chmean1[j] /= count;
-		chmean2[j] /= count;
-		chmean3[j] /= count;
-		chmean4[j] /= count;
-
-		chcorr[j] /= count - 1;
-
-		double tmp = chmean1[j]*chmean1[j];
-
-		chmean[j] = chmean1[j];
-		chstd[j] = chmean2[j]-tmp;
-		
-		if (chstd[j] > 0.)
-		{
-			chskewness[j] = chmean3[j]-3.*chmean2[j]*chmean1[j]+2.*tmp*chmean1[j];
-			chkurtosis[j] = chmean4[j]-4.*chmean3[j]*chmean1[j]+6.*chmean2[j]*tmp-3.*tmp*tmp;
-
-			chkurtosis[j] /= chstd[j]*chstd[j];
-			chkurtosis[j] -= 3.;
-
-			chskewness[j] /= chstd[j]*std::sqrt(chstd[j]);
-
-			chcorr[j] -= tmp;
-			chcorr[j] /= chstd[j];
-		}
-		else
-		{
-			chstd[j] = 1.;
-			chkurtosis[j] = std::numeric_limits<float>::max();
-			chskewness[j] = std::numeric_limits<float>::max();
-
-			chcorr[j] = std::numeric_limits<float>::max();
-		}
-
-		chstd[j] = std::sqrt(chstd[j]);
-	}
-
-	/* calculate mean and std of chkurtosis and chskewness */
-	std::vector<float> kurtosis_sort(chkurtosis.begin(), chkurtosis.end());
-	std::nth_element(kurtosis_sort.begin(), kurtosis_sort.begin()+kurtosis_sort.size()/4, kurtosis_sort.end(), std::less<float>());
-	float kurtosis_q1 = kurtosis_sort[kurtosis_sort.size()/4];
-	std::nth_element(kurtosis_sort.begin(), kurtosis_sort.begin()+kurtosis_sort.size()/4, kurtosis_sort.end(), std::greater<float>());
-	float kurtosis_q3 =kurtosis_sort[kurtosis_sort.size()/4];
-	float kurtosis_R = kurtosis_q3-kurtosis_q1;
-
-	std::vector<float> skewness_sort(chskewness.begin(), chskewness.end());
-	std::nth_element(skewness_sort.begin(), skewness_sort.begin()+skewness_sort.size()/4, skewness_sort.end(), std::less<float>());
-	float skewness_q1 = skewness_sort[skewness_sort.size()/4];
-	std::nth_element(skewness_sort.begin(), skewness_sort.begin()+skewness_sort.size()/4, skewness_sort.end(), std::greater<float>());
-	float skewness_q3 =skewness_sort[skewness_sort.size()/4];
-	float skewness_R = skewness_q3-skewness_q1;
-
-	std::vector<double> corr_sort(chcorr.begin(), chcorr.end());
-	std::nth_element(corr_sort.begin(), corr_sort.begin()+corr_sort.size()/4, corr_sort.end(), std::less<float>());
-	double corr_q1 = corr_sort[corr_sort.size()/4];
-	std::nth_element(corr_sort.begin(), corr_sort.begin()+corr_sort.size()/4, corr_sort.end(), std::greater<float>());
-	double corr_q3 = corr_sort[corr_sort.size()/4];
-	double corr_R = corr_q3-corr_q1;
-
-	float thresig = vm["zapthre"].as<float>();
-
-	for (long int j=0; j<nchans; j++)
-	{
-		if (chkurtosis[j]>=kurtosis_q1-thresig*kurtosis_R && \
-			chkurtosis[j]<=kurtosis_q3+thresig*kurtosis_R && \
-			chskewness[j]>=skewness_q1-thresig*skewness_R && \
-			chskewness[j]<=skewness_q3+thresig*skewness_R && \
-			chcorr[j]>=corr_q1-thresig*corr_R && \
-			chcorr[j]<=corr_q3+thresig*corr_R)
-		{
-			chweight[j] = 1.;
-		}
-	}
+	stat.get_stat();
 
 	std::ofstream outfile;
 	outfile.open(rootname + ".dat", std::ios::binary);
-	outfile.write((char *)chweight.data(), chweight.size() * sizeof(float));
-	outfile.write((char *)chmean.data(), chmean.size() * sizeof(float));
-	outfile.write((char *)chstd.data(), chstd.size() * sizeof(float));
+	outfile.write((char *)stat.chweight.data(), stat.chweight.size() * sizeof(float));
+	outfile.write((char *)stat.chmean.data(), stat.chmean.size() * sizeof(float));
+	outfile.write((char *)stat.chstd.data(), stat.chstd.size() * sizeof(float));
 	outfile.close();
 
 	delete reader;
